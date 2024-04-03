3e17dfeb67955cd8d107bf96242b702cdc5cc510
Jorio27 <1080277@hr.nl>
2024-04-03 14:57:16 +0200

    Unit tests

---
+++ MuseumTests/TestProgram.cs
@@ -0,0 +1,12 @@
+namespace Program;
+
+[TestClass]
+public class TestProgram
+{
+    [DataTestMethod]
+    [DataRow("10101001", "1")]
+    public void AddToTourInvalidID(string customerid, string tourid)
+    {
+        AddToTour
+    }
+}
