f3a0551b77a53cb3923804ee41a94c1edc382c7d
Robin1082623 <1082623@hr.nl>
2024-04-17 15:08:36 +0200

    Voeg toe dat je de eerste 5 rondleidingen kan zien. Voeg ook toe dat je niet te veel hoeft te typen.

--- MuseumTours/Logic/Tours.cs
+++ MuseumTours/Logic/Tours.cs
@@ -38,26 +38,55 @@ public class Tours
     Console.WriteLine($"We hebben geen tour kunnen vinden met het ingevoerde nummer:{tourid}.");
     return false;
   }
-  public static void ShowAvailableTours()
+  public static void ShowAvailableTours(int FiveOrAll)
   {
-    List<Tours> listOfTours = DataAccess.ReadJsonTours();
-    int Count = 0;
-    Console.WriteLine("Dit zijn de nog beschikbare rondleidingen voor vandaag:");
-    bool touratleast = false;
-    foreach (Tours tour in listOfTours)
+    if (FiveOrAll == 1)
     {
-      if (tour.Time > DateTime.Now && tour.Spots > 0)
+      List<Tours> listOfTours = DataAccess.ReadJsonTours();
+      int Count = 0;
+      Console.WriteLine("Dit zijn de nog beschikbare rondleidingen voor vandaag:");
+      bool touratleast = false;
+      foreach (Tours tour in listOfTours)
       {
-        Count++;
-        string timeString = tour.Time.ToString("HH:mm");
-        Console.WriteLine($"{tour.ID}. starttijd: {timeString} | duur: 40 minuten | beschikbare plekken: {tour.Spots}");
-        touratleast = true;
+        if (tour.Time > DateTime.Now && tour.Spots > 0)
+        {
+          Count++;
+          string timeString = tour.Time.ToString("HH:mm");
+          Console.WriteLine($"{tour.ID}. starttijd: {timeString} | duur: 40 minuten | beschikbare plekken: {tour.Spots}");
+          touratleast = true;
+        }
+      }
+      if (touratleast == false)
+      {
+        Console.WriteLine("Er zijn op het moment geen beschikbare rondleidingen.");
+        Program.Main();
       }
     }
-    if (touratleast == false)
+    else // Alleen voor de eerste 5 laten zien.
     {
-      Console.WriteLine("Er zijn op het moment geen beschikbare rondleidingen.");
-      Program.Main();
+      List<Tours> listOfTours = DataAccess.ReadJsonTours();
+      int Count = 0;
+      Console.WriteLine("Dit zijn de nog beschikbare rondleidingen voor vandaag:");
+      bool touratleast = false;
+      foreach (Tours tour in listOfTours)
+      {
+        if (tour.Time > DateTime.Now && tour.Spots > 0)
+        {
+          Count++;
+          string timeString = tour.Time.ToString("HH:mm");
+          Console.WriteLine($"{tour.ID}. starttijd: {timeString} | duur: 40 minuten | beschikbare plekken: {tour.Spots}");
+          touratleast = true;
+          if (Count == 5)
+          {
+            break;
+          }
+        }
+      }
+      if (touratleast == false)
+      {
+        Console.WriteLine("Er zijn op het moment geen beschikbare rondleidingen.");
+        Program.Main();
+      }
     }
   }
 }
