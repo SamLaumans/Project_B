5924c16d35fc0cd829ef7c36cab15c8d619f4f14
Nickraaihr <1081708@hr.nl>
2024-05-06 15:55:40 +0200

    fix dat tournummer in tour list moet staan

--- MuseumTours/Logic/Guide.cs
+++ MuseumTours/Logic/Guide.cs
@@ -23,76 +23,113 @@ public class Guide
         }
         return false;
     }
-    public static void CheckEmployeeID()
+public static void CheckEmployeeID()
+{
+    string pattern = @"[^0-9]";
+    bool answerValid = false;
+
+    while (!answerValid)
     {
-        string pattern = @"[^0-9]";
-        bool Answer = false;
-        while (Answer == false)
+        Console.WriteLine("Scan de barcode op uw badge, toets het nummer onder de barcode in of toets 'q' om terug te gaan naar het begin.");
+        string employeeID = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(employeeID))
         {
-            Console.WriteLine("Scan de barcode op uw badge, toets het nummer onder de barcode in of toets 'q' om terug te gaan naar het begin.");
-            string Employee_ID = Console.ReadLine();
-            if (Employee_ID == null)
+            Console.WriteLine($"De door u ingevulde code was: '{employeeID}'. De code kan niet leeg zijn.");
+        }
+        else if (employeeID == "q")
+        {
+            Program.Main();
+        }
+        else if (employeeID.Length != 6 || Regex.IsMatch(employeeID, pattern))
+        {
+            Console.WriteLine($"De door u ingevulde code was: '{employeeID}'. De code bestaat altijd uit 6 cijfers.");
+        }
+        else
+        {
+            Guide guide = new Guide(employeeID);
+            if (guide.CheckIfGuideInList(employeeID))
             {
-                Console.WriteLine($"De door u ingevulde code was: '{Employee_ID}'. De code kan niet leeg zijn.");
-            }
-            else if (Employee_ID == "q")
-            {
-                Program.Main();
-            }
-            else if (Employee_ID.Count() != 6)
-            {
-                Console.WriteLine($"De door u ingevulde code was: '{Employee_ID}'. De code bestaat altijd uit 6 cijfers.");
-            }
-            else if (Regex.IsMatch(Employee_ID, pattern))
-            {
-                Console.WriteLine($"De door u ingevulde code was: '{Employee_ID}'. De code bestaat altijd uit 6 cijfers.");
+                Tours.ShowToursToGuide("../../../Tourslist.Json");
+                answerValid = ChooseTour();
             }
             else
             {
-                Guide guide = new Guide(Employee_ID);
-                if (guide.CheckIfGuideInList(Employee_ID))
-                {
-                    Answer = true;
-                    Tours.ShowToursToGuide("../../../Tourslist.Json");
-                    bool answerValid = false;
-                    while (answerValid == false)
-                    {
-                        Console.WriteLine("Voer het rondleidingsnummer in waarvan u de deelnemers wilt zien of toets 'q' om terug te gaan naar het begin.");
-                        string ChosenTour = Console.ReadLine();
-                        if (ChosenTour == "q")
-                        {
-                            Program.Main();
-                        }
-                        else
-                        {
-                        Tours.ShowChosenTour(ChosenTour);
-                        bool ChosenOption = false;
-                        while (ChosenOption == false)
-                        {
-                            Console.WriteLine("Om terug te gaan naar het overzicht van tours toets (b) om terug te gaan naar het beginscherm toets (q)");
-                            string GuideChoice = Console.ReadLine();
-                            if (GuideChoice == null)
-                            {
-                                Console.WriteLine("Voer een geldige optie in.");
-                            }
-                            else if (GuideChoice == "b")
-                            {
-                                Tours.ShowToursToGuide("../../../Tourslist.Json");
-                                break;
-                            }
-                            else if (GuideChoice == "q")
-                            {
-                                Program.Main();
-                            }
-                        }
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"De door u ingevulde code was: '{Employee_ID}', Probeer alstublieft opnieuw.");
-                }
+                Console.WriteLine($"De door u ingevulde code was: '{employeeID}', Probeer alstublieft opnieuw.");
             }
         }
     }
+}
+
+public static bool ChooseTour()
+{
+    bool answerValid = false;
+
+    while (!answerValid)
+{
+    Console.WriteLine("Voer het rondleidingsnummer in waarvan u de deelnemers wilt zien of toets 'q' om terug te gaan naar het begin.");
+    string chosenTour = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(chosenTour))
+    {
+        Console.WriteLine($"Uw invoer was '{chosenTour}'. Voer een geldige tournummer in.");
+    }
+    else if (chosenTour == "q")
+    {
+        Program.Main();
+    }
+    else
+    {
+        List<Tours> listOfTours = DataAccess.ReadJsonTours();
+        bool tourFound = false;
+
+        foreach (Tours tour in listOfTours)
+        {
+            if (tour.ID == chosenTour)
+            {
+                Tours.ShowChosenTour(chosenTour);
+                answerValid = ChooseOption();
+                tourFound = true;
+                break;
+            }
+        }
+
+        if (!tourFound)
+        {
+            Console.WriteLine($"Het opgegeven rondleidingsnummer '{chosenTour}' is niet geldig. Voer een geldig tournummer in.");
+        }
+    }
+}
+
+
+    return answerValid;
+}
+
+public static bool ChooseOption()
+{
+    bool chosenOption = false;
+
+    while (!chosenOption)
+    {
+        Console.WriteLine("Om terug te gaan naar het overzicht van tours toets (b) om terug te gaan naar het beginscherm toets (q)");
+        string guideChoice = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(guideChoice))
+        {
+            Console.WriteLine("Voer een geldige optie in.");
+        }
+        else if (guideChoice == "b")
+        {
+            Tours.ShowToursToGuide("../../../Tourslist.Json");
+            break;
+        }
+        else if (guideChoice == "q")
+        {
+            Program.Main();
+        }
+    }
+
+    return chosenOption;
+}
+
 }
