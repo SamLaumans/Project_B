d1876f45e8f5e5e1405155a2a440f5347b317497
Jorio27 <1080277@hr.nl>
2024-05-08 15:29:25 +0200

    Add basic worlds

---
+++ MuseumTours/Logic/IWorld.cs
@@ -0,0 +1,14 @@
+namespace Program;
+
+public interface IWorld
+{
+    DateTime Now { get; }
+
+    void WriteLine(string line);
+
+    string ReadLine();
+
+    string ReadAllText(string path);
+
+    void WriteAllText(string path, string contents);
+}
